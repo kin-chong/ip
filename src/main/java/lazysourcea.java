2227b96b10a3256aa0b51b02987e9bc28f245c63
kin-chong <kinchong_low@yahoo.com>
2025-08-19 00:02:36 +0800

    implemented level 0 greet, exit

--- src/main/java/lazysourcea.java
+++ src/main/java/lazysourcea.java
@@ -6,6 +6,21 @@ public class lazysourcea {
                 + "| | (_| |/ /| |_| \\__ \\ (_) | |_| | | | (_|  __/ (_| |\n"
                 + "|_|\\__,_/___|\\__, |___/\\___/ \\__,_|_|  \\___\\___|\\__,_|\n"
                 + "             |___/\n";
-                System.out.println("Hello from\n" + logo);
+        String greeting = "hi. i'm lazysourcea.\nwhat do you want to do?";
+        String greetingline = "-----------------------";
+
+        String bye = "bye.";
+        String byeline = "----";
+
+
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println(greetingline);
+        System.out.println(greeting);
+        System.out.println(greetingline);
+
+        System.out.println(byeline);
+        System.out.println(bye);
+        System.out.println(byeline);
     }
 }
