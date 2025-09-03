7cfdd01b04bbbb3427a715301e759a724d7a7115
kin-chong <kinchong_low@yahoo.com>
2025-09-03 19:23:35 +0800

    Change indent of switch statement to fit coding standard

--- src/main/java/lazysourcea/lazysourcea.java
+++ src/main/java/lazysourcea/lazysourcea.java
@@ -166,92 +166,92 @@ public class lazysourcea {
         });
 
         switch (parsed.type) {
-            case BYE:
-                ui.showBye();
-                this.isExit = true;
-                break;
-            case LIST:
-                taskList.listTasks(line -> sb.append(line).append('\n'));
-                break;
-            case MARK:
-                try {
-                    int index = parser.parseIndex(parsed.arg, taskList.listSize());
-                    Task t = taskList.getTask(index);
-                    t.isDone();
-                    storage.save(taskList.asList());
-                    ui.showMarked(t);
-                } catch (NumberFormatException | IndexOutOfBoundsException e) {
-                    ui.showError("invalid task number. use: mark <number>");
-                }
-                break;
-            case UNMARK:
-                try {
-                    int index = parser.parseIndex(parsed.arg, taskList.listSize());
-                    Task t = taskList.getTask(index);
-                    t.isNotDone();
-                    storage.save(taskList.asList());
-                    ui.showUnmarked(t);
-                } catch (NumberFormatException | IndexOutOfBoundsException e) {
-                    ui.showError("invalid task number. use: unmark <number>");
-                }
-                break;
-            case TODO:
-                if (parsed.arg.isEmpty()) {
-                    ui.showError("tsk.. todo description cannot empty.\nuse: todo <desc>");
-                } else {
-                    Task todo = new Todo(parsed.arg);
-                    taskList.addTask(todo);
-                    storage.save(taskList.asList());
-                    ui.showAdded(todo, taskList.listSize());
-                }
-                break;
-            case DEADLINE:
-                try {
-                    Parser.DeadlineArgs d = parser.parseDeadlineArgs(parsed.arg);
-                    Task deadline = new Deadline(d.desc, d.by);
-                    taskList.addTask(deadline);
-                    storage.save(taskList.asList());
-                    ui.showAdded(deadline, taskList.listSize());
-                } catch (Exception e) {
-                    ui.showError("oi.. invalid deadline format.\nuse: deadline <desc> /by <time>"
-                            + "\naccepted: yyyy-MM-dd (e.g., 2019-10-15) or d/M/yyyy (e.g., 2/12/2019)");
-                }
-                break;
-            case EVENT:
-                try {
-                    Parser.EventArgs ev = parser.parseEventArgs(parsed.arg);
-                    Task event = new Event(ev.desc, ev.from, ev.to);
-                    taskList.addTask(event);
-                    storage.save(taskList.asList());
-                    ui.showAdded(event, taskList.listSize());
-                } catch (Exception e) {
-                    ui.showError("oi.. invalid event format.\nuse: event <desc> /from <time> /to <time>");
-                }
-                break;
-            case DELETE:
-                try {
-                    int index = parser.parseIndex(parsed.arg, taskList.listSize());
-                    Task removed = taskList.removeTask(index);
-                    storage.save(taskList.asList());
-                    ui.showDeleted(removed, taskList.listSize());
-                } catch (NumberFormatException e) {
-                    ui.showError("oi.. give valid task number pls.\nUsage: delete <number>");
-                } catch (IndexOutOfBoundsException e) {
-                    ui.showError("task number out of range lah.");
-                }
-                break;
-            case FIND:
-                if (parsed.arg.isEmpty()) {
-                    ui.showError("usage: find <keyword>");
-                } else {
-                    ui.showFindResults(taskList, parsed.arg);
-                }
-                break;
-            case HELP:
-                ui.showHelp();
-                break;
-            default:
-                ui.showUnknownOrEmpty(parsed.raw == null ? "" : parsed.raw);
+        case BYE:
+            ui.showBye();
+            this.isExit = true;
+            break;
+        case LIST:
+            taskList.listTasks(line -> sb.append(line).append('\n'));
+            break;
+        case MARK:
+            try {
+                int index = parser.parseIndex(parsed.arg, taskList.listSize());
+                Task t = taskList.getTask(index);
+                t.isDone();
+                storage.save(taskList.asList());
+                ui.showMarked(t);
+            } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                ui.showError("invalid task number. use: mark <number>");
+            }
+            break;
+        case UNMARK:
+            try {
+                int index = parser.parseIndex(parsed.arg, taskList.listSize());
+                Task t = taskList.getTask(index);
+                t.isNotDone();
+                storage.save(taskList.asList());
+                ui.showUnmarked(t);
+            } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                ui.showError("invalid task number. use: unmark <number>");
+            }
+            break;
+        case TODO:
+            if (parsed.arg.isEmpty()) {
+                ui.showError("tsk.. todo description cannot empty.\nuse: todo <desc>");
+            } else {
+                Task todo = new Todo(parsed.arg);
+                taskList.addTask(todo);
+                storage.save(taskList.asList());
+                ui.showAdded(todo, taskList.listSize());
+            }
+            break;
+        case DEADLINE:
+            try {
+                Parser.DeadlineArgs d = parser.parseDeadlineArgs(parsed.arg);
+                Task deadline = new Deadline(d.desc, d.by);
+                taskList.addTask(deadline);
+                storage.save(taskList.asList());
+                ui.showAdded(deadline, taskList.listSize());
+            } catch (Exception e) {
+                ui.showError("oi.. invalid deadline format.\nuse: deadline <desc> /by <time>"
+                        + "\naccepted: yyyy-MM-dd (e.g., 2019-10-15) or d/M/yyyy (e.g., 2/12/2019)");
+            }
+            break;
+        case EVENT:
+            try {
+                Parser.EventArgs ev = parser.parseEventArgs(parsed.arg);
+                Task event = new Event(ev.desc, ev.from, ev.to);
+                taskList.addTask(event);
+                storage.save(taskList.asList());
+                ui.showAdded(event, taskList.listSize());
+            } catch (Exception e) {
+                ui.showError("oi.. invalid event format.\nuse: event <desc> /from <time> /to <time>");
+            }
+            break;
+        case DELETE:
+            try {
+                int index = parser.parseIndex(parsed.arg, taskList.listSize());
+                Task removed = taskList.removeTask(index);
+                storage.save(taskList.asList());
+                ui.showDeleted(removed, taskList.listSize());
+            } catch (NumberFormatException e) {
+                ui.showError("oi.. give valid task number pls.\nUsage: delete <number>");
+            } catch (IndexOutOfBoundsException e) {
+                ui.showError("task number out of range lah.");
+            }
+            break;
+        case FIND:
+            if (parsed.arg.isEmpty()) {
+                ui.showError("usage: find <keyword>");
+            } else {
+                ui.showFindResults(taskList, parsed.arg);
+            }
+            break;
+        case HELP:
+            ui.showHelp();
+            break;
+        default:
+            ui.showUnknownOrEmpty(parsed.raw == null ? "" : parsed.raw);
         }
 
         return sb.toString().trim();
